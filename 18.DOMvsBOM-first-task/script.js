43b3c954409404e538d8a41b0cb04932a6ec4ea8
Leila Nazarli <116263067+leilanazarli@users.noreply.github.com>
2023-04-23 08:54:59 -0700

    Add files via upload

---
+++ 18.DOMvsBOM-first-task/script.js
@@ -0,0 +1,38 @@
+let startbtn=document.querySelector(".start")
+let stopbtn=document.querySelector(".stop")
+let result=document.querySelector("#result")
+let resetbtn=document.querySelector(".reset")
+let incrementbtn=document.querySelector(".increment")
+let decrementbtn=document.querySelector(".decrement")
+let incrementvalueinput=document.querySelector(".incrementvalueinput")
+let incrementvaluebtn=document.querySelector(".incrementvaluebtn")
+let counter=0;
+let interval;
+startbtn.addEventListener("click" , function(){
+   interval =setInterval(()=>{
+    counter++;
+    result.innerText=counter;
+   },1000)
+})
+stopbtn.addEventListener("click",function(){
+    clearInterval(interval)
+
+})
+resetbtn.addEventListener("click", function(){
+    result.innerText=0
+    counter=0
+    clearInterval(interval)
+})
+incrementbtn.addEventListener("click",function(){
+        result.innerText=counter++
+})
+decrementbtn.addEventListener("click",function(){
+        result.innerText=--counter
+})
+var i=0;
+function buttonClick(){
+    document.getElementById("incrementvalueinput").value=++i*4;
+}
+
+
+
